24cbb4349fcc7576a4fe7489d6a40a2cbfa6dc8e
Phatkervn <phatkervn@gmail.com>
2022-08-03 19:41:45 +0700

    hss

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path , include
-from .views import home_index , view_app , search , app
+from .views import home_index , view_app , search , app ,about
 urlpatterns = [
     path("",home_index,name="homepage"),
     path("view/<int:ID>", view_app),
     path("search" , search),
-    path("app/<str:type>" , app)
+    path("app/<str:type>" , app),
+    path("about",about)
 ]
--- home/views.py
+++ home/views.py
@@ -7,29 +7,65 @@ from .models import App
 def home_index(request):
 
     app_hot = App.objects.filter(appType__contains="hot")[:3]
-    app_new = App.objects.filter(appType__contains="new")
-    app_mobile = App.objects.filter(appType__contains="mobile")
-    app_pc = App.objects.filter(appType__contains="pc")
-    return render(request=request, template_name="home/index.html",context={"app_new" : app_new , "app_Mobile":app_mobile ,"app_Pc":app_pc})
+    game_mod = App.objects.filter(appType__contains="game_mod")
+    game_pc = App.objects.filter(appType__contains="game_pc")
+    game_mobile = App.objects.filter(appType__contains="game_mobile")
+    app_mobile = App.objects.filter(appType__contains="app_mobile")
+    tool = App.objects.filter(appType__contains="tool")
+    return render(
+        request=request, 
+        template_name="home/index.html",
+        context={
+            "app_hot" : app_hot , 
+            "game_pc":game_pc ,
+            "game_mod":game_mod ,
+            "game_mobile":game_mobile,
+            "app_mobile" : app_mobile ,
+            "tool":tool,
+        }
+    )
 
 
 def view_app(request,ID):
     app = App.objects.filter(pk=ID)
-    return render(request=request, template_name="home/appview.html",context={"app" : app})
+    return render(
+        request=request, 
+        template_name="home/appview.html",
+        context={
+            "app" : app
+        }
+    )
 
 def search(request):
     if request.method == "POST":
         query = request.POST.dict().get("searchQuery")
         #resultObj = App.objects.filter(Q(appName__search=str(query)))
-        return render(request,template_name="home/search.html",context={})
+        return render(
+            request,
+            template_name="home/search.html",
+            context={
+
+            }
+        )
 
 
 def app(request,type):
     app_new = App.objects.filter(appType__contains=str(type))
-    return render(request,template_name="home/app.html" , context={"apps": app_new , "type" : type})
+    return render(
+        request,
+        template_name="home/app.html" , 
+        context={
+            "apps": app_new , 
+            "type" : type
+        }
+    )
 
-def aubout(request):
-    return render(request,template_name="home/about.html")
+def about(request):
+    return render(
+        request,
+        template_name="home/about.html"
+    )
+
 def auto_Up(request):
     if request.method == "POST":
         pass
